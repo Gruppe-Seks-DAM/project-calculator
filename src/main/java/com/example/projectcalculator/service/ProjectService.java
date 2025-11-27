4c72f88146204bd59ebbfc26dd2c769e89467750
git-arian <195733915+git-arian@users.noreply.github.com>
2025-11-27 10:25:43 +0100

    feat(projects): implement US1 create project
    - add controller, service, repository, model and DTO
    - repository KeyHolder to return generated ID for later
    - add createProjectForm.html

--- src/main/java/com/example/projectcalculator/service/ProjectService.java
+++ src/main/java/com/example/projectcalculator/service/ProjectService.java
@@ -1,4 +1,23 @@
 package com.example.projectcalculator.service;
 
+import com.example.projectcalculator.dto.ProjectDto;
+import com.example.projectcalculator.model.Project;
+import com.example.projectcalculator.repository.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProjectService {
+    private final ProjectRepository repo;
+
+    public ProjectService(ProjectRepository repo) {
+        this.repo = repo;
+    }
+
+    public long create(ProjectDto dto) {
+        Project p = new Project();
+        p.setName(dto.getName());
+        p.setDescription(dto.getDescription());
+        p.setDeadline(dto.getDeadline());
+        return repo.create(p);
+    }
 }
